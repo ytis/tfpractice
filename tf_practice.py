a44f4d02798b45643aaf849cdb3d9099d7db4ec3
都築勇祐 <yusuke@MacBook-Air-de-tsudzukiisamuyuu.local>
2018-08-19 23:32:08 +0900

    add new file

--- tf_practice.py
+++ tf_practice.py
@@ -1,1 +1,19 @@
-// file to practice tensorflow
+# file to practice tensorflow
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print (tf.__version__)
+
+# import data
+fashion_ds = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+#class names
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
